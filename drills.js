0fda3ef9afa8d161d7c1b20bd51270307e916989
DanielJSottile <danielsottile@gmail.com>
2020-03-17 16:02:11 -0400

    added code thing for changing later

--- drills.js
+++ drills.js
@@ -72,3 +72,28 @@ for (let i = 0; i < newArray.length; i++) {
   }
 }
 
+// 6
+
+function decode(codedSentence) {
+  const wordArray = codedSentence.split(' ');
+  let decodedMessage = '';
+  for (let i = 0; i < wordArray.length; i++) {
+
+    if (wordArray[i][0] === 'a') {
+      decodedMessage += wordArray[i][1];
+    }
+    else if (wordArray[i][0] === 'b') {
+      decodedMessage += wordArray[i][2];
+    }
+    else if (wordArray[i][0] === 'c') {
+      decodedMessage += wordArray[i][3];
+    }
+    else if (wordArray[i][0] === 'd') {
+      decodedMessage += wordArray[i][4];
+    }
+    else {
+      decodedMessage += ' ';
+    }
+  }
+  return decodedMessage;
+}
